3d79c4774daa61968211676f458bbcce18d896c3
everson-cardoso <everson.cardoso82@gmail.com>
2024-01-25 13:30:45 -0300

    primeiro commit do meu projeto React

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,20 @@
+import logo from '../../imagens/logo.svg';
+import './estilo.css';
+
+function Logo(){
+    return (
+        <div className='logo'>
+            <img 
+                src={logo} 
+                alt='logo'
+                className='logo-img'
+            ></img>
+            <p><strong> Alura Books</strong></p>
+        </div> 
+    )
+}
+export default Logo
+
+
+
+
